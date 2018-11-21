44cf4087a223ff606e5d328481bacbaeac3f19c0
kei-iwai-koji <iw.kj.16@gmail.com>
2018-11-21 19:13:12 +0900

    editview

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,4 @@
 class MessagesController < ApplicationController
 
-  def index
-    @messages = Message.all
-  end
 
-  def create
-    @message = Message.create(body: message_params[:body], image: message_params[:image])
-  end
-
-  private
-  def message_params
-    params_permit(:body, :image)
-  end
 end
