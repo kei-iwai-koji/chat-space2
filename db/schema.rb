2fc442fccdce5d304c9707146ef432a246186a05
kei-iwai-koji <iw.kj.16@gmail.com>
2018-11-23 14:36:47 +0900

    create model
    user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181121033942) do
+ActiveRecord::Schema.define(version: 20181123053416) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -32,6 +32,19 @@ ActiveRecord::Schema.define(version: 20181121033942) do
     t.datetime "updated_at",               null: false
     t.integer  "group_id",                 null: false
     t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",                                null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "members", "groups"
