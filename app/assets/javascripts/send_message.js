6b008ec0e5afdc6a82bf666b4807973b6cfbd644
kei-iwai-koji <iw.kj.16@gmail.com>
2018-11-28 20:16:23 +0900

    Edit file
    jsファイル　create.json.jbuilder の編集
    ビューファイルの修正

--- app/assets/javascripts/send_message.js
+++ app/assets/javascripts/send_message.js
@@ -1,8 +1,63 @@
 $(function() {
-  $('.form__submit').on('click', function(e) {
+  function buildHTML(message) {
+    if (message.image && message.content) {
+      console.log("aaa")
+      var html = `<div class="message">
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                        ${message.date}
+                      </div>
+                    </div>
+                    <div class="lower-meesage">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                      <image src="${message.image}" class="lower-message__image" alt="image" width="300" height="300">
+                    </div>
+                  </div>`
+    } else if (message.content) {
+      console.log("bbb")
+      var html = `<div class="message">
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                        ${message.date}
+                      </div>
+                    </div>
+                    <div class="lower-meesage">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
+    } else {
+      console.log("ccc")
+      var html = `<div class="message">
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                        ${message.date}
+                      </div>
+                      <image src="${message.image}" class="lower-message__image" alt="image" width="300" height="300">
+                    </div>
+                  </div>`
+    }
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
+    console.log(url)
+    console.log(formData)
     $.ajax({
       url: url,
       type: "POST",
@@ -10,5 +65,16 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data) {
+      console.log(data)
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.form__message').val('')
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 500);
+    })
+    .fail(function() {
+      alert('error');
+    })
   })
-})
+});
